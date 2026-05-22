5faefc39ff529075590bc0dff656e3e02df22d88
jojo <pch468@naver.com>
2026-05-22 10:32:15 +0900

    test(mobile): WebView 셸 테스트 인프라 추가

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: "jest-expo",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testMatch: ["**/__tests__/**/*.test.ts", "**/__tests__/**/*.test.tsx"],
+  transformIgnorePatterns: [
+    "node_modules/(?!((jest-)?react-native|@react-native|expo(nent)?|@expo(nent)?/.*|@expo/.*|expo-router|react-navigation|@react-navigation/.*|react-native-webview))",
+  ],
+};
